bc955fd1aa8a8bbeecbe05458ef16f010c8a55f1
Dakuur <103331483+Dakuur@users.noreply.github.com>
2024-05-27 00:06:21 +0200

    Update branchAndBound.py

--- PythonSalesMan/branchAndBound.py
+++ PythonSalesMan/branchAndBound.py
@@ -5,6 +5,7 @@ import queue
 import dijkstra
 import graph_lib as gl
 from typing import List, Tuple, Dict
+from copy import deepcopy, copy
 
 # SalesmanTrackBranchAndBound1 ===================================================
 
@@ -25,14 +26,51 @@ def distances_and_paths(g: gl.Graph, visits: gl.Visits) -> Tuple[dict, dict]:
     """
     distances = {}
     paths = {}
-    for vertex in visits.Vertices[:-1]:
+    for vertex in visits.Vertices[
+        :-1
+    ]:  # skip the last vertex (no exiting paths from it)
         dists_v, paths_v = dijkstra.Dijkstra(g, vertex)
         distances[vertex.Name] = dists_v
         paths[vertex.Name] = paths_v
     return distances, paths
 
 
-def build_track(g: gl.Graph, path: list, best_paths: dict):
+def min_max_levels(dist_dict: dict, path: list, visits: gl.Visits) -> float:
+    """
+    Returns the minimum level of the vertices in the path
+    :param dist_dict: dict with the distances between all vertices
+    :param path: list of vertices
+    :param visits: Visits
+    :return: minimum level
+    """
+    to_visit = [
+        v.Name for v in visits.Vertices if (v not in path and v != visits.Vertices[-1])
+    ]
+
+    heuristics = dict()
+
+    for i in to_visit:
+        maximum_d = 0
+        minimum_d = math.inf
+        for k, v in dist_dict.items():
+            if i == k:
+                continue
+            if v[i] > maximum_d:
+                maximum_d = v[i]
+            if v[i] < minimum_d:
+                minimum_d = v[i]
+        heuristics[i] = (minimum_d, maximum_d)
+
+    return heuristics
+
+
+def build_track(g: gl.Graph, path: list, best_paths: dict) -> gl.Track:
+    """
+    Builds the track (all vertices and edges) from the best paths between the vertices to be visited (ordered)
+    :param g: Graph
+    :param path: list of vertices to be visited
+    :param best_paths: dict with the best paths between all vertices to be visited
+    """
     track = graph.Track(g)
     [
         track.AddLast(e)
@@ -43,10 +81,14 @@ def build_track(g: gl.Graph, path: list, best_paths: dict):
 
 
 def SalesmanTrackBranchAndBound2(g: gl.Graph, visits: gl.Visits):
-    distances, best_paths = distances_and_paths(g, visits)
+    dist_dict, paths_dict = distances_and_paths(g, visits)
 
-    result = build_track(g, visits.Vertices, best_paths)
-    return result
+    heuristics = min_max_levels(dist_dict, [], visits)
+
+    # result = build_track(g, path, paths_dict)
+    # return result
+
+    raise Exception("Funció de l'apartat 2 no implementada")
 
 
 # SalesmanTrackBranchAndBound3 ===================================================
